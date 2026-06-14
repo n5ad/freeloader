72412387db2f8b6a06ee728970facfc836538b2d
n5ad <n5ad@att.net>
2026-06-14 12:38:30 -0500

    Remove htmlspecialchars from filename output

--- freeloader_delete.php
+++ freeloader_delete.php
@@ -7,9 +7,9 @@
 
 <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['file'])) { $filename = basename($_POST['file']); $path = "/my_uploads/" . $filename;
     
-    if (file_exists($path) && unlink($path)) { echo " " . htmlspecialchars($filename) . " deleted.";
+    if (file_exists($path) && unlink($path)) { echo " " . ($filename) . " deleted.";
     } else {
-        echo " Failed to delete " . htmlspecialchars($filename);
+        echo " Failed to delete " . ($filename);
     }
 } else {
     echo "Invalid request.";
